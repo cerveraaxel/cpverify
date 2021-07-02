3662c09298429d131a74b0f591befd9cf2e57888
Axel Cervera <a.cervera@cpanel.net>
2021-07-02 13:29:26 -0500

    Added ip validation

--- cpverify.py
+++ cpverify.py
@@ -1,22 +1,32 @@
 #!/usr/local/bin/python3.9
-
+import socket
 import argparse
 import urllib.request
 
 #Argparse
 parser = argparse.ArgumentParser(
-        description="cPanel license IP validation tool")
+        description="cPanel license IP validation tool", usage="[IP Address]")
 parser.add_argument('-ip', required=True, help="IP Address to check", dest="ip")
 args = parser.parse_args()
 ip_addr = args.ip
 url = 'https://verify.cpanel.net/app/verify?ip=' + ip_addr + '&nohtml=1'
 
+def is_ipv4():
+    try:
+        socket.inet_pton(socket.AF_INET, ip_addr)
+        return True
+    except:
+        socket.error
+        print("Not a valid IP address!")
+        return False
+
 def main():
-    print("License data for IP address:" + ip_addr + "\n")
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        html = str(html.decode('utf-8'))
-        print(html)
+    if is_ipv4():
+        print("License data for IP address:" + ip_addr + "\n")
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            html = str(html.decode('utf-8'))
+            print(html)
 
 if __name__ == '__main__':
     main()
