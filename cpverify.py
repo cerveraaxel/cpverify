5498f9d3fbd7bf75aac65e3e4cf6b08f7de55271
Axel Cervera <a.cervera@cpanel.net>
2021-07-14 19:51:19 -0500

    Added vars to class and removed argparse for simplicity

--- cpverify.py
+++ cpverify.py
@@ -1,19 +1,19 @@
 #!/usr/local/bin/python3.9
 import socket
-import argparse
+import sys
 import urllib.request
 
-#Argparse
-parser = argparse.ArgumentParser(
-        description="cPanel license IP validation tool", usage="[IP Address]")
-parser.add_argument('-ip', required=True, help="IP Address to check", dest="ip")
-args = parser.parse_args()
-ip_addr = args.ip
-url = 'https://verify.cpanel.net/app/verify?ip=' + ip_addr + '&nohtml=1'
+class Vars:
+    if len(sys.argv) > 1:
+        ip_addr = sys.argv[1]
+    else:
+        print("Please provide an IP")
+        exit()
+    url = 'https://verify.cpanel.net/app/verify?ip=' + ip_addr + '&nohtml=1'
 
 def is_ipv4():
     try:
-        socket.inet_pton(socket.AF_INET, ip_addr)
+        socket.inet_pton(socket.AF_INET, Vars.ip_addr)
         return True
     except:
         socket.error
@@ -22,12 +22,12 @@ def is_ipv4():
 
 def main():
     if is_ipv4():
-        print("License data for IP address:", ip_addr + "\n")
-        with urllib.request.urlopen(url) as response:
+        #print("License data for IP address:", ip_addr + "\n")
+        with urllib.request.urlopen(Vars.url) as response:
             html = response.read()
             html = str(html.decode('utf-8'))
             print(html)
-            print(url.split('&')[0])
+            print(Vars.url.split('&')[0])
 
 if __name__ == '__main__':
     main()
